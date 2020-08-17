d97687c5734457dab423de04e6c2ab1690601307
JiunsLee <jiunsr@gmail.com>
2020-08-17 22:49:32 +0800

    1.2

--- src/views/download.vue
+++ src/views/download.vue
@@ -5,36 +5,25 @@
                 <img src="./../assets/img/qianbao_03.jpg" alt="">
             </div>
             <div class="walletText">
-                <p class="credible">收币宝<span style="color: black; font-size: 30px;">钱包</span></p>
-                <p class="text">去中心化、安全、可信钱包</p>
-                <div class="downloadCenter">
-                    <div class="downloadOverall">
-                        <div class="downloadImg">
-                            <p class="version">WIndows版</p>
-                            <img src="./../assets/img/xiazai_03.png" alt="">
-                        </div>
-                    </div>
-                    <div class="downloadOverall" style="background-color: #fff;">
-                        <div class="downloadImg">
-                            <p class="version" style="color: black;">Mac版</p>
-                        </div>
-                    </div>
-                    <div class="downloadOverall" style="background-color: #fff;">
-                        <div class="downloadImg">
-                            <p class="version" style="color: black;">Linux版</p>
-                        </div>
-                    </div>
-                    <div class="downloadOverall" style="background-color: #fff;">
-                        <div class="downloadImg">
-                            <p class="version" style="color: black;">iOS版</p>
-                        </div>
-                    </div>
-                    <div class="downloadOverall" style="background-color: #fff;">
-                        <div class="downloadImg">
-                            <p class="version" style="color: black;">Android版</p>
-                        </div>
-                    </div>
-                </div>
+                <div class="credible">收币宝<span style="color: black; font-size: 30px;">钱包</span></div>
+                <div class="text">去中心化、安全、可信钱包</div>                   
+                    <v-container class="downloadCenter">
+                        <v-row>
+                            <v-col md="4">
+                                <div class="downloadOverall">
+                                    <v-btn class="ma-2" color="#4f6ef7" dark  large="true" style="width: 200px; font-size: 18px; font-weight: bold !important; height: 60px; margin-bottom:20px; text-transform: none;">windows版
+                                        <v-icon size="20" dark right>{{mdiDownload}}</v-icon>
+                                    </v-btn>
+
+                                    <v-btn class="ma-2" color="#4f6ef7" dark  large="true" style="width: 200px; font-size: 18px; font-weight: bold !important; height: 60px; margin-left: 30px; margin-bottom:20px; text-transform: none;">Mac版</v-btn>
+                                    <v-btn class="ma-2" color="#4f6ef7" dark  large="true" style="width: 200px; font-size: 18px; font-weight: bold !important; height: 60px; margin-bottom:20px; text-transform: none;">Linux版</v-btn>
+                                    <v-btn class="ma-2" color="#4f6ef7" dark  large="true" style="width: 200px; font-size: 18px; font-weight: bold !important; height: 60px; margin-left: 30px; margin-bottom:20px; text-transform: none;">iOS版</v-btn>
+                                    <v-btn class="ma-2" color="#4f6ef7" dark  large="true" style="width: 200px; font-size: 18px; font-weight: bold !important; height: 60px; text-transform: none;">Android版</v-btn>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-container>                  
+                        
             </div>
         </div>
         <div class="bottom">
@@ -57,9 +46,12 @@
 </template>
 
 <script>
+import { mdiDownload } from '@mdi/js';
 export default {
 	data: function () {
-		return {}
+		return {
+            mdiDownload,
+        }
 	},
 	methods: { },
 	metaInfo () {
@@ -71,85 +63,62 @@ export default {
 <style lang="stylus" scoped>
 .download{
     .wallet{
-        height: 900px;
-        background-color: #fff;
-        display: flex;
+        height 900px
+        background-color #fff
+        display flex
 
         .walletImg{
-            padding: 92px 87px 156px 147px;
+            padding 92px 87px 156px 147px
             img{
-                width: 900px;
-                height: 675px;
+                width 900px
+                height 675px
             }
         }
         .walletText{
-            padding-top: 230px;
+            padding-top 230px
             .text{
-                font-size: 20px;
-                letter-spacing: 5px;
-                padding-bottom: 65px;
+                font-size 20px
+                letter-spacing 5px
+                padding 20px 0 65px
             }
             .credible{
-                font-size: 50px;
-                font-weight: bold;
-                color: #4f6ef7;
-                letter-spacing: 5px;
+                font-size 50px
+                font-weight bold
+                color #4f6ef7
+                letter-spacing 5px
             }
             .downloadCenter{
-                display: flex;
-                flex-wrap: wrap;
-                flex-basis: auto;
-                width: 67%;
                 .downloadOverall{
-                    width: 202px;
-                    height: 60px;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    margin: 0 30px 30px 0;
-                    box-shadow: 0 4px 10px #e4e4e4;
-                    background-color: #fff;
-                    border-radius: 10px;
-                    background-color: #4f6ef7;
-                    .downloadImg{
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        .version{
-                            font-size: 20px;
-                            font-weight: bold;
-                            color: #fff;
-                            padding-right: 10px;
-                        }
-                        img{
-                            width: 23px;
-                            height: 26px;
-                        }
+                    width 435px;
+                    margin-right 0;
+                    img{
+                        width 23px
+                        height 26px
                     }
-                }
+                }                
             }
         }
     }
 }
 .bottom{
-    background-color: #000829;
+    background-color #000829
     .bottomCenter{
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        display flex
+        justify-content center
+        align-items center
         .bottomImg{
-            padding: 15px 5px 5px;
+            padding 15px 5px 5px
             display flex
             img{
-                width: 27px;
-                height: 27px;
+                width 27px
+                height 27px
             }
         }
     }
     .siteName{
-        text-align: center;
-        font-size: 12px;
-        color: #fff;
+        text-align center
+        font-size 12px
+        color #fff
         padding-bottom 3px
     }
 }
